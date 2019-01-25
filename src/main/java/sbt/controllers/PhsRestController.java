4ec0e4db2fc6e5c18b88b99960d32db2eca8bc5d
Chenko16 <Durachenko-serg@yandex.ru>
2019-01-25 16:43:11 +0300

    Шаблон приложения

---
+++ src/main/java/sbt/controllers/PhsRestController.java
@@ -0,0 +1,12 @@
+package sbt.contrllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PhsRestController {
+
+    /*@Autowired
+    public PhsRestController() throws Exception {;}*/
+}
---
+++ src/main/java/sbt/controllers/PhsViewController.java
@@ -0,0 +1,21 @@
+package sbt.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.security.Principal;
+
+@Controller
+public class PhsViewController {
+
+    /*@Autowired
+    public PhsViewController() throws Exception { ;}*/
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model, Principal principal) {
+        return "index";
+    }
+
+}
