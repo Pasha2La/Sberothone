ae569225e959c4adac3bab9ca35e9bcbdca99812
serbar <Sergey521972>
2019-01-26 00:51:04 +0300

    change package name

--- src/main/java/sbt/controllers/PhsRestController.java
+++ src/main/java/sbt/controllers/PhsRestController.java
@@ -1,6 +1,5 @@
-package sbt.contrllers;
+package sbt.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
