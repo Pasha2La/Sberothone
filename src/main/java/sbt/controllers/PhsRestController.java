745e4b066572ba12c8aeb20f185e7cef9bbfa86c
Pasha2La <Pasha2La71@gmail.com>
2019-01-26 21:37:07 +0300

    added login in restController, improved authService

--- src/main/java/sbt/controllers/PhsRestController.java
+++ src/main/java/sbt/controllers/PhsRestController.java
@@ -1,9 +1,17 @@
 package sbt.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import sbt.auth.AuthService;
+import sbt.data.LoginInfo;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by durachenko-sv on 26.01.2019.
@@ -14,14 +22,20 @@ public class PhsRestController {
     private static final Logger logger = Logger.getLogger(PhsRestController.class);
 
     @Autowired
-    public PhsRestController() throws Exception {
-
-    }
+    AuthService authService;
 
     @RequestMapping("/login")
-    public String login(@RequestParam(value = "login") String loginStr,
-                        @RequestParam(value = "password") String passwordStr) {
-        String token = null;
-        return token;
+    public String login(LoginInfo loginInfo, HttpServletResponse response) {
+        try {
+            String token = authService.authenticate(loginInfo);
+            Cookie jwt = new Cookie("jwt", token);
+            response.addCookie(jwt);
+            JSONObject resp = new JSONObject();
+            resp.append("code",200);
+            return resp.toString();
+        } catch (Exception e) {
+            logger.error("error while doing login...", e);
+            return "{\"code\":500,\"error\":\"" + e.getMessage() + "\"}";
+        }
     }
 }
