31da2ce67b1f5aa73f19e321065dbfe8b905ee8f
shiftuia <shiftuia@gmail.com>
2019-01-26 22:35:10 +0300

    isHealthy

--- src/main/java/sbt/dao/model/Receipt.java
+++ src/main/java/sbt/dao/model/Receipt.java
@@ -8,14 +8,16 @@ public class Receipt {
     private String coockingTime;
     private Long calories;
     private Long category;
+    private Boolean isHealthy;
 
-    public Receipt(Long id, String name, String description, String coockingTime, Long calories, Long category) {
+    public Receipt(Long id, String name, String description, String coockingTime, Long calories, Long category, Boolean isHealthy) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.coockingTime = coockingTime;
         this.calories = calories;
         this.category = category;
+        this.isHealthy = isHealthy;
     }
 
     @Override
@@ -27,6 +29,7 @@ public class Receipt {
                 ", coockingTime='" + coockingTime + '\'' +
                 ", calories=" + calories +
                 ", category=" + category +
+                ", isHealthy=" + isHealthy +
                 '}';
     }
 
@@ -34,47 +37,55 @@ public class Receipt {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCoockingTime() {
-        return coockingTime;
-    }
-
-    public Long getCalories() {
-        return calories;
-    }
-
-    public Long getCategory() {
-        return category;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCoockingTime() {
+        return coockingTime;
     }
 
     public void setCoockingTime(String coockingTime) {
         this.coockingTime = coockingTime;
     }
 
+    public Long getCalories() {
+        return calories;
+    }
+
     public void setCalories(Long calories) {
         this.calories = calories;
     }
 
+    public Long getCategory() {
+        return category;
+    }
+
     public void setCategory(Long category) {
         this.category = category;
+    }
+
+    public Boolean getHealthy() {
+        return isHealthy;
+    }
+
+    public void setHealthy(Boolean healthy) {
+        isHealthy = healthy;
     }
 }
