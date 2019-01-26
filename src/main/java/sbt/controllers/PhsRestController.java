dca056e5ff132575d9747d1db914a326752bb14e
sergeitelnow <sergeitelnow@gmail.com>
2019-01-26 18:22:38 +0300

    fixed controller

--- src/main/java/sbt/controllers/PhsRestController.java
+++ src/main/java/sbt/controllers/PhsRestController.java
@@ -2,6 +2,9 @@ package sbt.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,6 +22,8 @@ public class PhsRestController {
         return token;
     }
 
-    /*@Autowired
-    public PhsRestController() throws Exception {;}*/
+    @Autowired
+    public PhsRestController() throws Exception {
+
+    }
 }
