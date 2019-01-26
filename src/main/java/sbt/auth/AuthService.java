fd9699c0830f40ba168993fbe368c3482668fb49
sergeitelnow <sergeitelnow@gmail.com>
2019-01-26 22:52:15 +0300

    logout

--- src/main/java/sbt/auth/AuthService.java
+++ src/main/java/sbt/auth/AuthService.java
@@ -1,8 +1,8 @@
 package sbt.auth;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.apache.log4j.Logger;
-import com.auth0.jwt.JWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import sbt.dao.model.Account;
@@ -27,9 +27,7 @@ public class AuthService {
         user.setExpireDate(new Timestamp(0));
         //ToDo request to DB for user Account
 
-        String token = null;
-        token = genJWT(user);
-        return token;
+        return genJWT(user);
     }
 
     private String genJWT(Account user) throws UnsupportedEncodingException {
--- src/main/java/sbt/controllers/PhsRestController.java
+++ src/main/java/sbt/controllers/PhsRestController.java
@@ -1,10 +1,11 @@
 package sbt.controllers;
 
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.*;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sbt.auth.AuthService;
 import sbt.data.LoginInfo;
 
@@ -20,20 +21,40 @@ public class PhsRestController {
     private static final Logger logger = Logger.getLogger(PhsRestController.class);
 
     @Autowired
-    AuthService authService;
+    private AuthService authService;
 
     @RequestMapping("/login")
     public String login(@RequestBody LoginInfo loginInfo, HttpServletResponse response) {
         try {
             String token = authService.authenticate(loginInfo);
+
             Cookie jwt = new Cookie("jwt", token);
             response.addCookie(jwt);
-            JSONObject resp = new JSONObject();
-            resp.append("code",200);
-            return resp.toString();
         } catch (Exception e) {
             logger.error("error while doing login...", e);
             return "{\"code\":500,\"error\":\"" + e.getMessage() + "\"}";
         }
+
+        JSONObject resp = new JSONObject();
+        resp.append("code", 200);
+        return resp.toString();
+    }
+
+    @RequestMapping("/logout")
+    public String logout(@RequestBody LoginInfo info, HttpServletResponse response) {
+        try {
+            String token = authService.authenticate(info);
+
+            Cookie jwtRemove = new Cookie("jwt", token);
+            jwtRemove.setMaxAge(0);
+            response.addCookie(jwtRemove);
+        } catch (Exception e) {
+            logger.error("Can't logout user" + e.getMessage());
+            return "{\"code\":500,\"error\":\"" + e.getMessage() + "\"}";
+        }
+
+        JSONObject resp = new JSONObject();
+        resp.append("code", 200);
+        return resp.toString();
     }
 }
