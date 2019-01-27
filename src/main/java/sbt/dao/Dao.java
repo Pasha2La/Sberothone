4ce7d6230bf4b292c93a8721203b23fad31357e3
serbar <Sergey521972>
2019-01-27 05:14:24 +0300

    correct dao

--- src/main/java/sbt/dao/Dao.java
+++ src/main/java/sbt/dao/Dao.java
@@ -3,16 +3,11 @@ package sbt.dao;
 import sbt.dao.model.Account;
 import sbt.dao.model.Product;
 import sbt.dao.model.Receipt;
-import sbt.data.LoginInfo;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface Dao {
-    /*public List<Receipt> filteredSearch(List<String> products,
-                                        List<String> categories,
-                                        String healthy);
-*/
     List<Account> example();
 
 
@@ -23,6 +18,8 @@ public interface Dao {
     Optional<Receipt> getByIdReceipt(Long id);
 
     List<Receipt> getSortByView();
+
     List<Receipt> getAvailableReceipts(String healthy, String category);
+
     List<Receipt> getAvailableReceipts(List<Product> products);
 }
